2f883767d941bb31a7f51b8ecbce2d441080b68a
Sinan Keskin <sinan@kesk.in>
2019-05-06 17:37:08 +0300

    Fix app.import

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  included(app) {
+    this._super.included.apply(this, arguments);
+    // Notiflix 1.5.0
+    app.import('node_modules/notiflix/dist/notiflix-1.5.0.min.css');
+    app.import('node_modules/notiflix/dist/notiflix-1.5.0.min.js');
+  }
 };
