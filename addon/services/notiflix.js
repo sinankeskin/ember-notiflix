efba51321d05212a57169f969d805e38c875fdb0
Sinan Keskin <sinan@kesk.in>
2019-05-20 22:06:52 +0300

    Add jsdoc

--- addon/services/notiflix.js
+++ addon/services/notiflix.js
@@ -25,6 +25,12 @@ export default Service.extend({
     this.notiflix.Loading.Init(this.config['loading'] || {});
     this.notiflix.Confirm.Init(this.config['confirm'] || {});
   },
+  /**
+   * Base merge function for override initial settings.
+   *
+   * @param {'notify'|'report'|'loading'|'confirm'} type Type of function to call
+   * @param {object} options Options to override
+   */
   merge(type, options) {
     switch (type) {
       case 'notify':
@@ -43,18 +49,45 @@ export default Service.extend({
         break;
     }
   },
+  /**
+   * Notify functionality only merge function for override initial settings.
+   *
+   * @param {object} options Options to override
+   */
   notifyMerge(options) {
     this.notiflix.Notify.Merge(options);
   },
+  /**
+   * Report functionality only merge function for override initial settings.
+   *
+   * @param {object} options Options to override
+   */
   reportMerge(options) {
     this.notiflix.Notify.Merge(options);
   },
+  /**
+   * Loading functionality only merge function for override initial settings.
+   *
+   * @param {object} options Options to override
+   */
   loadingMerge(options) {
     this.notiflix.Notify.Merge(options);
   },
+  /**
+   * Confirm functionality only merge function for override initial settings.
+   *
+   * @param {object} options Options to override
+   */
   confirmMerge(options) {
     this.notiflix.Notify.Merge(options);
   },
+  /**
+   * Base notify function.
+   *
+   * @param {'success'|'failure'|'warning'|'info'} type Type of function to call
+   * @param {string} message Message to show
+   * @param {function} callback onClick callback function
+   */
   notify(type, message, callback) {
     switch (type) {
       case 'success':
@@ -74,18 +107,51 @@ export default Service.extend({
         break;
     }
   },
+  /**
+   * Success notify function
+   *
+   * @param {string} message Message to show
+   * @param {function} callback onClick callback function
+   */
   notifySuccess(message, callback) {
     this.notiflix.Notify.Success(message, callback);
   },
+  /**
+   * Failure notify function
+   *
+   * @param {string} message Message to show
+   * @param {function} callback onClick callback function
+   */
   notifyFailure(message, callback) {
     this.notiflix.Notify.Failure(message, callback);
   },
+  /**
+   * Warning notify function
+   *
+   * @param {string} message Message to show
+   * @param {function} callback onClick callback function
+   */
   notifyWarning(message, callback) {
     this.notiflix.Notify.Warning(message, callback);
   },
+  /**
+   * Info notify function
+   *
+   * @param {string} message Message to show
+   * @param {function} callback onClick callback function
+   */
   notifyInfo(message, callback) {
     this.notiflix.Notify.Info(message, callback);
   },
+  /**
+   * Base report function.
+   *
+   * @param {'success'|'failure'|'warning'|'info'} type Type of function to call
+   * @param {string} title Title to show
+   * @param {string} message Message to show
+   * @param {string} btnText Button text on screen
+   * @param {function} callback onClick callback function
+   */
   report(type, title, message, btnText, callback) {
     switch (type) {
       case 'success':
@@ -105,18 +171,56 @@ export default Service.extend({
         break;
     }
   },
+  /**
+   * Success report function
+   *
+   * @param {string} title Title to show
+   * @param {string} message Message to show
+   * @param {string} btnText Button text on screen
+   * @param {function} callback onClick callback function
+   */
   reportSuccess(title, message, btnText, callback) {
     this.notiflix.Report.Success(title, message, btnText, callback);
   },
+  /**
+   * Failure report function
+   *
+   * @param {string} title Title to show
+   * @param {string} message Message to show
+   * @param {string} btnText Button text on screen
+   * @param {function} callback onClick callback function
+   */
   reportFailure(title, message, btnText, callback) {
     this.notiflix.Report.Failure(title, message, btnText, callback);
   },
+  /**
+   * Warning report function
+   *
+   * @param {string} title Title to show
+   * @param {string} message Message to show
+   * @param {string} btnText Button text on screen
+   * @param {function} callback onClick callback function
+   */
   reportWarning(title, message, btnText, callback) {
     this.notiflix.Report.Warning(title, message, btnText, callback);
   },
+  /**
+   * Info report function
+   *
+   * @param {string} title Title to show
+   * @param {string} message Message to show
+   * @param {string} btnText Button text on screen
+   * @param {function} callback onClick callback function
+   */
   reportInfo(title, message, btnText, callback) {
     this.notiflix.Report.Info(title, message, btnText, callback);
   },
+  /**
+   * Base loading function.
+   *
+   * @param {'standard'|'hourglass'|'circle'|'arrows'|'dots'|'pulse'|'custom'} type Type of function to call
+   * @param {string} message Message to show
+   */
   loading(type, message) {
     switch (type) {
       case 'standard':
@@ -145,33 +249,89 @@ export default Service.extend({
         break;
     }
   },
+  /**
+   * Standard loading function
+   *
+   * @param {string} message Message to show
+   */
   loadingStandard(message) {
     this.notiflix.Loading.Standard(message);
   },
+  /**
+   * Hourglass loading function
+   *
+   * @param {string} message Message to show
+   */
   loadingHourglass(message) {
     this.notiflix.Loading.Hourglass(message);
   },
+  /**
+   * Circle loading function
+   *
+   * @param {string} message Message to show
+   */
   loadingCircle(message) {
     this.notiflix.Loading.Circle(message);
   },
+  /**
+   * Arrows loading function
+   *
+   * @param {string} message Message to show
+   */
   loadingArrows(message) {
     this.notiflix.Loading.Arrows(message);
   },
+  /**
+   * Dots loading function
+   *
+   * @param {string} message Message to show
+   */
   loadingDots(message) {
     this.notiflix.Loading.Dots(message);
   },
+  /**
+   * Pulse loading function
+   *
+   * @param {string} message Message to show
+   */
   loadingPulse(message) {
     this.notiflix.Loading.Pulse(message);
   },
+  /**
+   * Custom loading function
+   *
+   * @param {string} message Message to show
+   */
   loadingCustom(message) {
     this.notiflix.Loading.Custom(message);
   },
+  /**
+   * Change loading function,
+   * changes the message on screen with new message
+   *
+   * @param {string} message Message to show
+   */
   loadingChange(message) {
     this.notiflix.Loading.Change(message);
   },
+  /**
+   * Remove loading function,
+   * removes the loading screen immediately or after the timeout milliseconds later.
+   *
+   * @param {number} timeout
+   */
   loadingRemove(timeout) {
     this.notiflix.Loading.Remove(timeout);
   },
+  /**
+   * Base confirm function
+   *
+   * @param {string} title Title to show
+   * @param {string} message Message to show
+   * @param {string} okBtnText OK button text on screen
+   * @param {string} cancelBtnText Cancel button text on screen
+   * @param {function} callback onClick callback function
+   */
   confirm(title, message, okBtnText, cancelBtnText, callback) {
     this.notiflix.Confirm.Show(
       title,
