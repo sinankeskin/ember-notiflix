1b473bc7faa7272d2e55b0ac77f5b1963d35cb2d
Sinan Keskin <sinan@kesk.in>
2019-07-02 13:36:05 +0300

    Update to 1.7.1

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
     this._super.included.apply(this, arguments);
 
     // Notiflix 1.7.0 CSS + JS
-    app.import('node_modules/notiflix/dist/notiflix-1.7.0.min.css');
-    app.import('node_modules/notiflix/dist/notiflix-1.7.0.min.js');
+    app.import('node_modules/notiflix/dist/notiflix-1.7.1.min.css');
+    app.import('node_modules/notiflix/dist/notiflix-1.7.1.min.js');
   }
 };
