a8f8df1dae571faeb8bf05878dfc39dd717d2f21
Sinan Keskin <sinan@kesk.in>
2019-07-23 14:55:08 +0300

    Update notiflix to 1.7.2

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ module.exports = {
   included(app) {
     this._super.included.apply(this, arguments);
 
-    // Notiflix 1.7.0 CSS + JS
-    app.import('node_modules/notiflix/dist/notiflix-1.7.1.min.css');
-    app.import('node_modules/notiflix/dist/notiflix-1.7.1.min.js');
+    // Notiflix 1.7.2 CSS + JS
+    app.import('node_modules/notiflix/dist/notiflix-1.7.2.css');
+    app.import('node_modules/notiflix/dist/notiflix-1.7.2.js');
   }
 };
