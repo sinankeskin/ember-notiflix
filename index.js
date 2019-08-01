cfad1be263f052a806e9f6ec01fcef4ade975078
Sinan Keskin <sinan@kesk.in>
2019-08-01 13:38:29 +0300

    Update notiflix to 1.8.0

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ module.exports = {
   included(app) {
     this._super.included.apply(this, arguments);
 
-    // Notiflix 1.7.2 CSS + JS
-    app.import('node_modules/notiflix/dist/notiflix-1.7.2.css');
-    app.import('node_modules/notiflix/dist/notiflix-1.7.2.js');
+    // Notiflix 1.8.0 CSS + JS
+    app.import('node_modules/notiflix/dist/notiflix-1.8.0.css');
+    app.import('node_modules/notiflix/dist/notiflix-1.8.0.js');
   }
 };
