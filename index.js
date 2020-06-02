c1f054e812d720936a69ae942e6bf9eda57e5ddb
Sinan Keskin <sinan@kesk.in>
2020-06-02 23:41:11 +0300

    Fix dist folder issue

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 'use strict';
 
-var paket = require('./package');
-
 module.exports = {
-  name: paket.name,
+  name: require('./package').name,
 
   included(app) {
     this._super.included.apply(this, arguments);
 
     // Notiflix CSS + JS
-    ['css', 'js'].forEach(ext => {
-      app.import(
-        `node_modules/notiflix/dist/Unminified/notiflix-${paket.dependencies.notiflix}.${ext}`
-      );
+    ['css', 'js'].forEach((ext) => {
+      app.import(`node_modules/notiflix/src/notiflix.${ext}`);
     });
-  }
+  },
 };
