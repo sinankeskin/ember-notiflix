7368577fe642d05079ccb6f5b620e6c61441a1d9
Sinan Keskin <sinan@kesk.in>
2020-10-19 17:56:30 +0300

    v3.19.0...v3.22.0

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ export default class MyComponent extends Component {
 Compatibility
 ------------------------------------------------------------------------------
 
-* Ember.js v3.12 or above
+* Ember.js v3.16 or above
 * Ember CLI v2.13 or above
 * Node.js v10 or above
 
--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,13 +1,13 @@
 import AddonDocsRouter, { docsRoute } from 'ember-cli-addon-docs/router';
-import config from './config/environment';
+import config from 'dummy/config/environment';
 
 export default class Router extends AddonDocsRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
 
-Router.map(function() {
-  docsRoute(this, function() {
+Router.map(function () {
+  docsRoute(this, function () {
     this.route('installation');
     this.route('usage');
     this.route('demo');
