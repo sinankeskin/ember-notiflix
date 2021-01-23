b3f060d1854d33413be0dcf8162adfb63dcd98df
Sinan Keskin <sinan@kesk.in>
2021-01-23 18:58:28 +0300

    Revert back to docs addon

--- tests/integration/components/ask-test.js
+++ tests/integration/components/ask-test.js
@@ -1,12 +1,13 @@
 import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | ask', function(hooks) {
+import { hbs } from 'ember-cli-htmlbars';
+import { render } from '@ember/test-helpers';
+import { setupRenderingTest } from 'ember-qunit';
+
+module('Integration | Component | ask', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
